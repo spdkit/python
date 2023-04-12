484168a29a7794c2f6864306033a2a4debe6ea68
Wenping Guo <ybyygu@gmail.com>
2023-04-12 15:20:07 +0800

    Import codes contributed by Mahuan for pymatgen

--- spdkit/__init__.py
+++ spdkit/__init__.py
@@ -1,9 +1,9 @@
 # [[file:../spdkit-python.note::fbe586a0][fbe586a0]]
-from .spdkit import *
+from spdkit import *
 
 
 def from_ase_atoms(ase_atoms):
-    """Construct a molecule object from ase Atoms"""
+    """Construct a Molecule object from ase Atoms"""
 
     import ase
 
@@ -19,21 +19,78 @@ def from_ase_atoms(ase_atoms):
     return mol
 
 
+def from_pmg_molecule(pmg_molecule):
+    """Construct a Molecule object from pymatgen Molecule object"""
+
+    import pymatgen
+
+    assert isinstance(
+        pmg_molecule, pymatgen.core.structure.Molecule
+    ), "not a pymatgen.core.structure.Molecule object!"
+
+    atoms = [from_pmg_site(s) for s in pmg_molecule]
+
+    mol = Molecule.from_atoms(atoms)
+    return mol
+
+
+def from_pmg_structure(pmg_structure):
+    """Construct a Molecule object from pymatgen Structure object"""
+
+    import pymatgen
+
+    assert isinstance(
+        pmg_structure, pymatgen.core.structure.Structure
+    ), "not a pymatgen.core.structure.Structure object!"
+
+    atoms = [from_pmg_site(s) for s in pmg_structure]
+
+    mol = Molecule.from_atoms(atoms)
+    # lattice object
+    lat = Lattice(pmg_struct.lattice.matrix.tolist())
+    mol.set_lattice(lat)
+    return mol
+
+
 def to_ase_atoms(mol: Molecule):
     """Construct an ase `Atoms` object from `Molecule`"""
     import ase
 
-    atoms = [ase.Atom(symbol=a.symbol(), position=a.position()) for _, a in mol.atoms()]
+    atoms = [to_ase_atom(a) for _, a in mol.atoms()]
     ase_atoms = ase.Atoms(atoms)
     lat = mol.get_lattice()
     if lat:
         ase_atoms.set_pbc(True)
-        ase_atoms.set_cell([lat.vector_a(), lat.vector_b(), lat.vector_c()])
+        ase_atoms.set_cell([lat.vector_a, lat.vector_b, lat.vector_c])
     return ase_atoms
 
 
+def to_pmg_molecule(mol: Molecule):
+    """Construct a pymatgen `Molecule` object from `Molecule`"""
+
+    import pymatgen
+
+    sites = [to_pmg_site(a) for _, a in mol.atoms()]
+    pmg_molecule = pymatgen.core.structure.Molecule.from_sites(sites)
+    return pmg_molecule
+
+
+def to_pmg_structure(mol: Molecule):
+    """Construct a pymatgen `Structure` object from `Molecule`"""
+
+    import pymatgen
+
+    lat = mol.get_lattice()
+    sites = [
+        to_pmg_periodic_site(a, [lat.vector_a, lat.vector_b, lat.vector_c])
+        for _, a in mol.atoms()
+    ]
+    pmg_structure = pymatgen.core.structure.Structure.from_sites(sites)
+    return pmg_structure
+
+
 def from_ase_atom(ase_atom) -> Atom:
-    """Construct a molecule object from ase Atom object"""
+    """Construct a Molecule object from ase Atom object"""
 
     import ase
 
@@ -41,11 +98,45 @@ def from_ase_atom(ase_atom) -> Atom:
     return Atom(ase_atom.symbol, list(ase_atom.position))
 
 
+def from_pmg_site(pmg_site):
+    """Construct an Atom object from pymatgen Site object"""
+
+    import pymatgen
+
+    assert isinstance(
+        pmg_site, pymatgen.core.sites.Site
+    ), "not a pymatgen.core.sites.Site object!"
+    return Atom(pmg_site.specie.symbol, list(pmg_site.coords))
+
+
 def to_ase_atom(atom: Atom):
     """Construct an ase `Atom` object from native `Atom`"""
     import ase
 
-    ase.Atom(symbol=atom.symbol(), position=atom.position())
+    return ase.Atom(symbol=atom.symbol, position=atom.position)
+
+
+def to_pmg_site(atom: Atom):
+    """Construct a pymatgein `Site` object from native `Atom`"""
+
+    import pymatgen
+
+    ele = pymatgen.core.periodic_table.Element(atom.symbol)
+    pmg_site = pymatgen.core.sites.Site(ele, coords=atom.position)
+    return pmg_site
+
+
+def to_pmg_periodic_site(atom: Atom, lattice):
+    """Construct a pymatgen `PeriodicSite` object from native `Atom`"""
+
+    import pymatgen
+
+    ele = pymatgen.core.periodic_table.Element(atom.symbol)
+    lat = pymatgen.core.lattice.Lattice(lattice)
+    pmg_site = pymatgen.core.sites.PeriodicSite(
+        ele, coords=atom.position, lattice=lat, coords_are_cartesian=True
+    )
+    return pmg_site
 # fbe586a0 ends here
 
 # [[file:../spdkit-python.note::ec59e65f][ec59e65f]]
