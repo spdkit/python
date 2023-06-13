03485f4ac2020659b06f56f4b579ae022cd1958d
Wenping Guo <ybyygu@gmail.com>
2023-06-13 09:53:15 +0800

    rebond in multiwfn scheme based covalent radii sum

--- src/lib.rs
+++ src/lib.rs
@@ -739,15 +739,24 @@ impl PyMolecule {
         Ok(n)
     }
 
-    #[pyo3(signature = (ignore_pbc=false, bond_tolerance=0.45))]
-    #[pyo3(text_signature = "(ignore_pbc=False, bond_tolerance=0.45)")]
+    #[pyo3(signature = (ignore_pbc=false, bond_tolerance=0.45, bonding_scheme=None))]
+    #[pyo3(text_signature = "(ignore_pbc=False, bond_tolerance=0.45, bonding_scheme=None)")]
     /// Recalculates all bonds in molecule based on interatomic
     /// distances and covalent radii. For periodic system, the bonds
-    /// are determined by applying miniumu image convention.
-    pub fn rebond(&mut self, ignore_pbc: bool, bond_tolerance: f64) {
-        std::env::set_var("GCHEMOL_REBOND_IGNORE_PBC", format!("{ignore_pbc}"));
-        std::env::set_var("GCHEMOL_REBOND_BOND_TOLERANCE", format!("{bond_tolerance}"));
-        self.inner.rebond();
+    /// are determined in miniumu image convention.
+    ///
+    /// # Parameters
+    /// * ignore_pbc: force to ignore periodicity when search nearest neighbors. The default is False.
+    /// * bonding_scheme: the bonding scheme for rebond. Avaialbe scheme includes jmol and multiwfn.
+    /// * bond_tolerance: the bonding tolerance used in jmol scheme.
+    pub fn rebond(&mut self, ignore_pbc: bool, bond_tolerance: f64, bonding_scheme: Option<String>) {
+        let mut options = Molecule::rebond_options();
+        options.ignore_pbc = ignore_pbc;
+        options.bond_tolerance = bond_tolerance;
+        if let Some(s) = bonding_scheme {
+            options.set_bonding_scheme(&s);
+        }
+        self.inner.rebond_with_options(&options);
     }
 
     /// Removes all existing bonds between atoms.
