dcff510abfe3d3ce43f756ffac1119c278b23016
Wenping Guo <ybyygu@gmail.com>
2024-02-19 17:13:47 +0800

    Use ExtxyzFile for read_lattice_extxyz

--- src/io.rs
+++ src/io.rs
@@ -4,7 +4,7 @@ use pyo3::prelude::*;
 // 45ba73aa ends here
 
 // [[file:../spdkit-python.note::4cfd5c0b][4cfd5c0b]]
-use super::{PyMolecule, PyMoleculeIter, PyLattice};
+use super::{PyLattice, PyMolecule, PyMoleculeIter};
 
 #[pyfunction]
 /// Read a list of `Molecule` from `path`. Returns an iterator over
@@ -32,7 +32,7 @@ pub fn guess_format_from_path(path: &str) -> Option<String> {
 /// Guess chemical file format from `path`
 #[pyfunction]
 pub fn read_lattice_extxyz(line: &str) -> Option<PyLattice> {
-    let inner = gchemol::io::read_lattice_extxyz(line)?;
+    let inner = gchemol::io::formats::ExtxyzFile::read_lattice(line)?;
     Some(PyLattice { inner })
 }
 
